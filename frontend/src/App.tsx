38c93a69078556dca84706e57ed8b66ba6b97e45
nsanchez1310 <nsanchez1310@gmail.com>
2022-07-13 00:18:05 -0300

    useState

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 
-import Header from "./components/Header"
-import SalesCard from "./components/SalesCard"
-import './index.css'
+import Header from "./components/Header";
+import SalesCard from "./components/SalesCard";
+import './index.css';
 
 
 function App() {
@@ -21,4 +21,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
