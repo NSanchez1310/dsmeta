4b6773dad56dde4834b62aba3358819aaf963973
nsanchez1310 <nsanchez1310@gmail.com>
2022-07-12 22:20:12 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
+import NotificationButton from "./components/NotificationButton"
 
 
 function App() {
- 
+
 
   return (
-   <h1>Olá Mundo Véio</h1>
+    <>
+      <h1>Olá Mundo Véio</h1>
+      <NotificationButton />
+    </>
   )
 }
 
